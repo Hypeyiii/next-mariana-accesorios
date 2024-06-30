0ad149e37316bc693938d10b8327e18b21e51350
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-30 04:40:05 -0600

    🪄 Add error when email is already taken

--- src/app/api/users/signUp.ts
+++ src/app/api/users/signUp.ts
@@ -6,12 +6,21 @@ export default async function signUp(email: string, password: string) {
   try {
     const client = await db.connect();
     const hashedPassword = await bcrypt.hash(password, 10);
+    const emailExists = await client.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+    if (emailExists.rows.length) {
+      throw new Error("Este email ya está en uso.");
+    }
     await client.query(
       "INSERT INTO users (id, email, password, created_at) VALUES ($1, $2, $3, NOW())",
       [randomUUID(), email, hashedPassword]
     );
   } catch (error) {
-    console.error("Failed to sign up:", error);
-    throw new Error("Failed to sign up.");
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+    throw new Error("Algo salió mal.");
   }
 }
