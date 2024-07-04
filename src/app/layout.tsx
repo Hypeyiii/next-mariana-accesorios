c11211a3c4bf4e177c23bdfdb2086b0abcecd602
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-07-04 01:59:35 -0600

    🛠️ Change layout to async

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Navbar } from "@/app/components/navbar";
 import Footer from "@/app/components/footer";
 import { CartProvider } from "@/app/context/CartContext";
 import { UserProvider } from "./context/UserContext";
+import getProducts from "./api/products/getProducts";
 
 export const metadata: Metadata = {
   title: "Mariana Accesorios",
@@ -12,17 +13,18 @@ export const metadata: Metadata = {
     "Mariana Accesorios | Everyday Fine Jewelry | Online Jewelry Shop",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const products = await getProducts();
   return (
     <html lang="en">
       <body className={inter.className}>
         <UserProvider>
           <CartProvider>
-            <Navbar />
+            <Navbar products={products} />
             {children}
             <Footer />
           </CartProvider>
